af7d3c547f31f0e5ff3a6be14517a6d1c6b8e7b0
zhangxin <xinzhang.hello@gmail.com>
2021-02-24 23:00:21 +0800

    test recipe

--- cookbook1/recipes/recipe2.rb
+++ cookbook1/recipes/recipe2.rb
@@ -1,15 +1,15 @@
-cookbook_file "/tmp/deploy-aws.sh" do
+cookbook_file "/home/ubuntu/deploy-aws.sh" do
   source "deploy-aws.sh"
   mode 0755
 end
 
-cookbook_file "/tmp/deploy" do
+cookbook_file "/home/ubuntu/deploy" do
   source "deploy"
   mode 0755
 end
 
 execute "deploy" do
-  user "root"
-  cwd "/tmp"
+  user "ubuntu"
+  cwd "/home/ubuntu"
   command "./deploy"
 end
